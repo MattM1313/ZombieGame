cf1de6a54d6e1f26fc4253ceceaf27bc3f1bb6ae
MattM <matt.mayo1313@gmail.com>
2014-03-10 20:05:08 -0400

    Player LookAt Mouse Script Base Done, Still has bugs

---
+++ ZombieSurvival/Assets/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+	public Transform t;
+	public Camera camera;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Plane playerPlane = new Plane(Vector3.up, t.position);
+		Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+		float hitLength = 0.0f;
+
+		if(playerPlane.Raycast(ray, out hitLength))
+		{
+			Vector3 target = ray.GetPoint(hitLength);
+			target = target - t.position;
+
+			Quaternion targetRotation = Quaternion.LookRotation(target);
+			t.rotation = targetRotation;
+		}
+	
+	}
+}
