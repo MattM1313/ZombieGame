90081593a4cc36d6923369966930a3795edede11
MattM1313 <matt.mayo1313@gmail.com>
2014-03-24 11:12:28 -0400

    Follow and Wander AI Working Seperatly, need to work on solution to use together

---
+++ ZombieSurvival/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform player;
+	public float moveSpeed;
+	public float minDist, maxDist;
+	public Transform EnemyFollow;
+
+
+
+	void Start () 
+	{
+	
+			
+	}
+	
+	void Update () 
+	{
+
+		transform.LookAt(player);
+
+		if(Vector3.Distance(transform.position, player.position) < maxDist)
+		{
+			//EnemyFollow.GetComponent<Wander>().enabled = false;
+			//EnemyFollow.GetComponent<FollowPlayer>().enabled = true;
+			transform.position += transform.forward * moveSpeed * Time.deltaTime;
+
+
+
+		}
+
+		else
+		{
+			//EnemyFollow.GetComponent<Wander>().enabled = true;
+			//EnemyFollow.GetComponent<FollowPlayer>().enabled = false;
+		}
+	
+	}
+}
